6e252f790c4a51312a81939431d92fe9cbb6977c
ParkerCF <110678158+ParkerCF@users.noreply.github.com>
2023-02-26 17:48:39 -0500

    updated blogRoutes

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -15,6 +15,24 @@ router.post('/', withAuth, async (req, res) => {
   }
 });
 
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const [blogData] = await Blog.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (blogData > 0) {
+      res.status(200).end();
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.delete('/:id', withAuth, async (req, res) => {
   try {
     const blogData = await Blog.destroy({
